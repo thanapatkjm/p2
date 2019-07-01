cd24a832153a176c13a49c2c08fef28d2bb51515
Dxcuer <thanapatkjm@hotmail.com>
2019-07-01 19:11:34 +0700

    urgent

---
+++ webapp/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path 
+
+app_name = 'webapp'
+urlpatterns = [
+    path('',views.home, name='home'),
+    path('login/',views.login, name='login'),
+    path('logout/',views.logout, name='logout'),
+    path('action/<str:id>',views.action, name='action'),
+    path('registration/',views.registration, name='regis'),
+]
